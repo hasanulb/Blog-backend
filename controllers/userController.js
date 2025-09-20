e2790b8a73f72f9a017d56a58df626da401a68e5
HASANUL BANNA <hasanulbanna@HASANULs-MacBook-Air.local>
2025-09-20 19:30:27 +0400

    user management

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 // GET all users
 const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find().select("-password");
+    const users = await User.find({ role: "member" }).select("-password");
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json({ message: "Server error", error: err.message });
